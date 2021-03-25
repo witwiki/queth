b8e137f81f4c9c52f9a0ef5a4a0ad77e848c7504
witwiki <witwiki@users.noreply.github.com>
2021-03-25 16:28:06 +1300

    web3 integration

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import Web3 from 'web3';
+const Web3 = require('web3');
+// import Web3 from 'web3';
+
+// Connect to Ethereum Blockchain here (or localhost)
+const web3 = new Web3('http://localhost:8545');
+
+
+
